d54d4dc9af42396fd66c52c63c8e660bc28edceb
yaqubadesola <yaqub.adesola@gmail.com>
2025-08-20 09:19:34 +0100

    Fixing deploy bug

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const repoName = "https://github.com/yaqubadesola/switch_assessment";
+const repoName = "https://yaqubadesola.github.io/switch_assessment";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
