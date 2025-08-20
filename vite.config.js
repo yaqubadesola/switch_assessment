b9f2210d45d12557c39c8b77013a1953c190a315
yaqubadesola <yaqub.adesola@gmail.com>
2025-08-20 09:13:58 +0100

    New configuration for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-//const repoName = "https://github.com/yaqubadesola/switch_assessment";
+const repoName = "https://github.com/yaqubadesola/switch_assessment";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: `/${repoName}/`,
+  base: `/${repoName}/`,
 });
