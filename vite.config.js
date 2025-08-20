99110905327b4755234a5fdefa7b409cc6df9ff6
yaqubadesola <yaqub.adesola@gmail.com>
2025-08-20 09:23:59 +0100

    Removing Github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const repoName = "https://yaqubadesola.github.io/switch_assessment";
+// const repoName = "https://yaqubadesola.github.io/switch_assessment";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`,
+  // base: `/${repoName}/`,
 });
